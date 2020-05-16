eec07d20b4020499c02dbabd227c13a0cae42fa2
Harry <harryng.gj@gmail.com>
2020-05-16 14:19:31 +0800

    Improving the Welcome Screen

--- App.js
+++ App.js
@@ -1,22 +1,28 @@
 import React from "react";
-import { StyleSheet, Platform, StatusBar } from "react-native";
+import { View, StyleSheet, Platform, StatusBar } from "react-native";
 import {
   useDimensions,
   useDeviceOrientation,
 } from "@react-native-community/hooks";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <WelcomeScreen />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  testContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,20 +1,27 @@
 import React from "react";
 import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
 import colors from "../config/colors";
+import AppButton from "../components/AppButton";
 
 function WelcomeScreen(props) {
   return (
     <ImageBackground
+      blurRadius={5}
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require("../assets/logo.png")} />
-        <Text>Sell What You Don't Need</Text>
+        <Text style={styles.tagline}>Sell What You Don't Need</Text>
       </View>
-
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <View style={styles.buttonsContainer}>
+        <AppButton title={"Login"} onPress={() => console.log("tapped")} />
+        <AppButton
+          title={"Register"}
+          color="secondary"
+          onPress={() => console.log("tapped")}
+        />
+      </View>
     </ImageBackground>
   );
 }
@@ -25,24 +32,24 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     alignItems: "center",
   },
-  loginButton: {
+  buttonsContainer: {
+    padding: 20,
     width: "100%",
-    height: 70,
-    backgroundColor: colors.primary,
-  },
-  registerButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: colors.secondary,
   },
   logo: {
     width: 100,
     height: 100,
+    borderRadius: 50,
   },
   logoContainer: {
     position: "absolute",
     top: 70,
     alignItems: "center",
+  },
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
   },
 });
 
