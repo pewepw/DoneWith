7768c0f6c9122e2069279cb44abbeba176614a47
Harry <harryng.gj@gmail.com>
2020-05-17 23:18:13 +0800

    Assets

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -7,10 +7,7 @@ import ListItem from "../components/ListItem";
 function ListingDetailsScreen(props) {
   return (
     <View>
-      <Image
-        style={styles.image}
-        source={require("../assets/background.jpg")}
-      />
+      <Image style={styles.image} source={require("../assets/jacket.jpg")} />
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red jacket for sale</AppText>
         <AppText style={styles.price}>$100</AppText>
