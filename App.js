1db25ed2816a2f6329a3b10c9062f35c03508f66
Harry <harryng.gj@gmail.com>
2020-05-24 02:57:55 +0800

    handling selections

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   StatusBar,
   TextInput,
   Text,
+  Switch,
 } from "react-native";
 import {
   useDimensions,
@@ -23,13 +24,38 @@ import ListItem from "./app/components/ListItem";
 import AccountSceen from "./app/screens/AccountSceen";
 import ListingsScreen from "./app/screens/ListingsScreen";
 import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
+
+const categories = [
+  {
+    label: "Furniture",
+    value: 1,
+  },
+  {
+    label: "Clothing",
+    value: 2,
+  },
+  {
+    label: "Cameras",
+    value: 3,
+  },
+];
 
 export default function App() {
   const [firstName, setFirstName] = useState("");
+  const [isNew, setIsNew] = useState(false);
+  const [category, setCategory] = useState();
 
   return (
     <BaseView>
-      <AppTextInput placeholder="Username" icon="email" />
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        data={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
     </BaseView>
   );
 }
