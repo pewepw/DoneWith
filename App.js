ef8d5874423cefa9b1c377fe84a837c3d726ac7d
Harry <harryng.gj@gmail.com>
2020-05-26 18:01:13 +0800

    The Touched State

--- App.js
+++ App.js
@@ -25,39 +25,14 @@ import AccountSceen from "./app/screens/AccountSceen";
 import ListingsScreen from "./app/screens/ListingsScreen";
 import AppTextInput from "./app/components/AppTextInput";
 import AppPicker from "./app/components/AppPicker";
-
-const categories = [
-  {
-    label: "Furniture",
-    value: 1,
-  },
-  {
-    label: "Clothing",
-    value: 2,
-  },
-  {
-    label: "Cameras",
-    value: 3,
-  },
-];
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
   const [firstName, setFirstName] = useState("");
   const [isNew, setIsNew] = useState(false);
   const [category, setCategory] = useState();
 
-  return (
-    <BaseView>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        data={categories}
-        icon="apps"
-        placeholder="Category"
-      />
-      <AppTextInput icon="email" placeholder="Email" />
-    </BaseView>
-  );
+  return <LoginScreen />;
 }
 
 const styles = StyleSheet.create({
